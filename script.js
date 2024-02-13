3c705b50fd9d28e71d99a6ffea3240303a221576
Amund Halgunset <amundhalgunset@gmail.com>
2024-02-13 12:43:56 +0100

    api mann

---
+++ script.js
@@ -0,0 +1,15 @@
+const apiUrl = 'https://api.noroff.dev/api/v1/rainy-days';
+
+fetch(apiUrl)
+  .then(response => {
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    return response.json();
+  })
+  .then(data => {
+    console.log(data);
+  })
+  .catch(error => {
+    console.error('Error fetching data:', error);
+  });

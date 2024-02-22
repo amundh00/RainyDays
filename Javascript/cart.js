77d54a7b4882c707a5527ffa4db26516f5e57651
Amund Halgunset <amundhalgunset@gmail.com>
2024-02-22 15:56:30 +0100

    føfy

--- Javascript/cart.js
+++ Javascript/cart.js
@@ -1,15 +1,15 @@
-import { listData } from "./utils.js"
+import { listData, cart } from "./utils.js";
 
 const outElement = document.getElementById("checkout-box");
 
-async function addedToCart() {
+async function addedToCart(cart) {
     try {
         const api = `https://v2.api.noroff.dev/rainy-days`;
         const response = await fetch(api);
         const data = await response.json();
 
         let filtered = data.data.filter((item)=>{
-            let id = product.id;
+            let id = item.id;
             return cart.includes(id);
         });
         if (filtered.length > 0) {
@@ -18,8 +18,9 @@ async function addedToCart() {
             outElement.innerHTML = `You have no products added to cart`;
         }
     } catch (error) {
+        console.error(error.message);
         outElement.innerHTML = `Could not fetch data`;
     }
 }
 
-addedToCart();
+addedToCart(cart);
